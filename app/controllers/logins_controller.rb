1f8b8c2974b577c5ebab5c1c70043d45840a1de6
Renée Cruz <reneebasscruz@gmail.com>
2019-09-12 23:54:52 -0400

    css

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,14 +1,14 @@
-class LoginsController < ApplicationController 
+class LoginsController < ApplicationController
 
     def new
     end
 
-    def create 
+    def create
         customer = Customer.find_by(email: params[:email])
         # barber = Barber.find_by(email: params[:email])
         # if customer
             if customer && customer.authenticate(params[:password])
-                session[:customer] = customer.id
+                session[:customer_id] = customer.id
                 redirect_to customer_path(customer.id)
             else
                 flash[:notification] = ["Either your Email or Password are incorrect."]
@@ -48,12 +48,12 @@ class LoginsController < ApplicationController
     end
 
     def destroy
-        if session[:customer_id] 
+        if session[:customer_id]
             session[:customer_id] = nil
             redirect_to new_login_path
         # elsif session[:barber_id]
         #     session[:barber_id]
         end
-        
+
     end
 end
